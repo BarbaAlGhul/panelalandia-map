f48d0ab8e229ccb4fc731f53fce23ed96d04c9be
BarbaAlGhul <anderson.stan5@gmail.com>
2019-08-14 17:15:50 -0300

    change image files to indexed png for world size reduction

---
+++ config.js
@@ -0,0 +1,127 @@
+var CONFIG = {
+  "maps": {
+    "world_isometric_day": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1565812338,
+        1565812338,
+        1565812338,
+        1565812338
+      ],
+      "maxZoom": 8,
+      "name": "PanelaLandia - Dia",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "PanelaLandia_isometric_t1",
+      "tileSize": 384,
+      "world": "PanelaLandia",
+      "worldName": "PanelaLandia",
+      "worldSeaLevel": 64
+    },
+    "world_isometric_night": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1565812338,
+        1565812338,
+        1565812338,
+        1565812338
+      ],
+      "maxZoom": 8,
+      "name": "PanelaLandia - Noite",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "PanelaLandia_isometric_t1",
+      "tileSize": 384,
+      "world": "PanelaLandia",
+      "worldName": "PanelaLandia",
+      "worldSeaLevel": 64
+    },
+    "world_topdown_day": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1565812338,
+        1565812338,
+        1565812338,
+        1565812338
+      ],
+      "maxZoom": 7,
+      "name": "PanelaLandia - Topdown overview",
+      "renderView": "topdown",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 6,
+      "tileSetGroup": "PanelaLandia_topdown_t3",
+      "tileSize": 288,
+      "world": "PanelaLandia",
+      "worldName": "PanelaLandia",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "world_isometric_day",
+    "world_isometric_night",
+    "world_topdown_day"
+  ],
+  "tileSetGroups": {
+    "PanelaLandia_isometric_t1": {
+      "maxZoom": 8,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    },
+    "PanelaLandia_topdown_t3": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 3
+    }
+  }
+};
